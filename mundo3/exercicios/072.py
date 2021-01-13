1845a509f1b9672280d1ba27cee9056d58c522ca
nosrefeg <gefersonaraujo00@outlook.com>
2021-01-12 23:19:28 -0300

    Exercício 72 - Número por extenso

---
+++ mundo3/exercicios/072.py
@@ -0,0 +1,10 @@
+zeroAVinte = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez',
+              'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezessete', 'dezoito', 'dezenove', 'vinte')
+numero = int(input('Digite número entre 0 e 20: '))
+while True:
+    while numero < 0 or numero > 20:
+        print('Número não está na lista, tente novamente!')
+        numero = int(input('Digite número entre 0 e 20: '))
+
+    print(f'Você digitou o número {zeroAVinte[numero]}')
+    break
