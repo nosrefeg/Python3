3cbf6b9a36d11ec51409f518ada30e4cf69319a0
nosrefeg <gefersonaraujo00@outlook.com>
2021-01-16 22:43:37 -0300

    Exercícios usando Listas

---
+++ mundo3/exercicios/079.py
@@ -0,0 +1,19 @@
+valores = []
+
+while True:
+
+    numero = int(input('Digite um número: '))
+    if numero not in valores:
+        valores.append(numero)
+        print('Valor adicionado!')
+    else:
+        print('Valor já existe, não adicionarei!')
+
+    confirmacao = input('Quer continuar? (S/N) ').strip().upper()
+    while confirmacao not in 'SN':
+        confirmacao = input('Quer continuar? (S/N) ').strip().upper()
+    if confirmacao == 'N':
+        break
+
+valores.sort()
+print(f'Os valores únicos digitados foram {valores}')
