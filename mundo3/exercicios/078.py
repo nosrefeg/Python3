aa5713b58cfca319e1cd4ce79398616e5e93dd30
nosrefeg <gefersonaraujo00@outlook.com>
2021-01-16 22:35:30 -0300

    Exercícios usando Listas

--- mundo3/exercicios/078.py
+++ mundo3/exercicios/078.py
@@ -1,5 +1,15 @@
 valores = []
 for i in range(0, 5):
     valores.append(int(input('Digite um número: ')))
+
 print(f'''Você digitou os valores {valores}
-O maior valor foi {max(valores)} na posição {valores.index(max(valores))} e o menor valor foi {min(valores)} na posição {valores.index(min(valores))}''')
+O maior valor foi {max(valores)} nas posições ''', end='')
+for i, v in enumerate(valores):
+    if v == max(valores):
+        print(f'{i} ', end='')
+
+print(
+    f'\nO menor valor foi {min(valores)} nas posições ', end='')
+for i, v in enumerate(valores):
+    if v == min(valores):
+        print(f'{i} ', end='')
