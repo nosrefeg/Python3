ffa78af921a64bd0c957f10df1e61184fc92160e
nosrefeg <gefersonaraujo00@outlook.com>
2021-01-20 00:21:06 -0300

    Exercícios usando Listas Compostas

---
+++ mundo3/exercicios/089.py
@@ -0,0 +1,29 @@
+alunos = []
+
+while True:
+    dados = [input('Digite o nome do aluno: '), [float(
+        input('Digite a primeira nota: ')), float(input('Digite a segunda nota: '))]]
+    alunos.append(dados[:])
+
+    confirm = input('Quer continuar? (S/N) ').strip().upper()
+    while confirm not in 'SN':
+        confirm = input('Quer continuar? (S/N) ').strip().upper()
+    if confirm == 'N':
+        print('==' * 30)
+        print(f'{"Nº":<10} {"Nome":^5} {"Média":>10}')
+        print('--' * 30)
+        for i in range(0, len(alunos)):
+            print(
+                f'{i:<10} {alunos[i][0]:^5} {(alunos[i][1][0] + alunos[i][1][1]) / 2:>10}')
+        print('--' * 30)
+        while True:
+            pos = int(input('Mostrar notas de qual aluno? (999 para parar) '))
+            while pos > len(alunos) - 1:
+                print('Aluno não cadastrado. Tente Novamente!')
+                pos = int(
+                    input('Mostrar notas de qual aluno? (999 para parar) '))
+            if pos == 999:
+                break
+            else:
+                print(f'Notas de {alunos[pos][0]} são {alunos[pos][1]}')
+        break
