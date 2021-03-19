8a9eb1015dc7a7e52574beafb10f524ce836b02c
nosrefeg <gefersonaraujo00@outlook.com>
2021-03-18 22:27:08 -0300

    Criando, inserindo, recuperando e deletando dados

--- DB/firstConnection.py
+++ DB/firstConnection.py
@@ -1,5 +1,6 @@
 import mysql.connector
 
+# Conectando ao MySQL e ao banco de dados
 banco = mysql.connector.connect(
     host="127.0.0.1",
     user="root",
@@ -9,5 +10,20 @@ banco = mysql.connector.connect(
 
 cursor = banco.cursor()
 
-cursor.execute(
-    'create table paes(id int not null auto_increment key, nome varchar(20) not null, preco float not null)')
+# Criando uma tabela
+# cursor.execute('create table paes(id int not null auto_increment key, nome varchar(20) not null, preco float not null)')
+
+# Inserindo na tabela
+# comandoSQL = 'INSERT INTO paes (nome, preco) VALUES (%s, %s)'
+# data = ('De açucar', '0.35')
+# cursor.execute(comandoSQL, data)
+# banco.commit()
+
+# Recuperando os dados da tabela
+# cursor.execute('SELECT * FROM paes WHERE preco>2.5')
+# informacaoDaTabela = cursor.fetchall()
+# print(informacaoDaTabela)
+
+# Deletando dados da tabela
+cursor.execute('DELETE FROM paes WHERE nome = "De açucar" ')
+banco.commit()
